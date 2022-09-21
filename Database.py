f35ebe2bdcb493ea955b30d1d24640cd2d7f1afd
Noa Hatuel <hatuelnoa@gmail.com>
2022-09-21 16:10:05 +0300

    Handled data base

---
+++ Database.py
@@ -0,0 +1,108 @@
+import pandas as pd
+import consts
+import os
+import ast
+
+index = 0
+#this function initializes the info in the file
+def initialize_file():
+    if os.stat(consts.FILE_NAME).st_size == 0:
+        events = {
+            consts.COL_INDEX: [],
+            consts.COL_LOCATION: [],
+            consts.COL_INFO: [],
+            consts.COL_TIME: [],
+            consts.COL_DATE: [],
+            consts.COL_PARTICIPANTS: []
+        }
+
+        df = pd.DataFrame(events)
+        df.to_csv(consts.FILE_NAME, mode='a', index = False ,header=True)
+
+
+#event: {location: "...", info: "...", time: "...", date: "...", participants: "..."}
+def add_event(event):
+    global index
+    df = pd.read_csv(consts.FILE_NAME)
+    # gets the row for the specific num
+    data_to_append = {consts.COL_INDEX: index,
+        consts.COL_LOCATION: [event[consts.COL_LOCATION]],
+        consts.COL_INFO: [event[consts.COL_INFO]],
+        consts.COL_TIME: [event[consts.COL_TIME]],
+        consts.COL_DATE: [event[consts.COL_DATE]],
+        consts.COL_PARTICIPANTS: [event[consts.COL_PARTICIPANTS]]
+    }
+    df = pd.DataFrame(data_to_append)
+    df.to_csv(consts.FILE_NAME, mode='a',index = False ,header=False)
+    index += 1
+
+
+def add_participants(event, participants):
+    df = pd.read_csv(consts.FILE_NAME)
+    index = get_index_by_info(event)
+    for item in participants:
+        event["participants"].append(item)
+    df.loc[index, consts.COL_PARTICIPANTS] = str(event["participants"])
+    df.to_csv(consts.FILE_NAME, mode='w', index=False, header=True)
+
+
+def read_events(location = ""):
+    is_location = True
+    if len(location) == 0:
+        is_location = False
+    data = pd.read_csv(consts.FILE_NAME)
+    wanted_events = get_records(data, location)
+    details = dict()
+    events = []
+    for event in wanted_events:
+        details = {}
+        details[consts.COL_INDEX] = event[consts.COL_INDEX]
+        details[consts.COL_LOCATION] = event[consts.COL_LOCATION]
+        details[consts.COL_INFO] = event[consts.COL_INFO]
+        details[consts.COL_TIME] = event[consts.COL_TIME]
+        details[consts.COL_DATE] = event[consts.COL_DATE]
+        details[consts.COL_PARTICIPANTS] = convert_to_list(event[consts.COL_PARTICIPANTS])
+        events.append(details)
+    return events
+
+
+def get_index_by_info(event):
+    index = -1
+    found = True
+    location = event[consts.COL_LOCATION]
+    info = event[consts.COL_INFO]
+    date = event[consts.COL_DATE]
+    events = read_events()
+    for item in events:
+        if item[consts.COL_LOCATION] == location and item[consts.COL_INFO] == info and item[consts.COL_DATE] == date:
+            index = item[consts.COL_INDEX]
+    return index
+
+
+def get_records(data, location = ""):
+    dicts = data.to_dict('records')
+    events = []
+    if len(location) > 0:
+        for item in dicts:
+            if item[consts.COL_LOCATION] == location:
+                events.append(item)
+        return events
+    else:
+        return dicts
+
+def convert_to_list(locations_str):
+    return ast.literal_eval(locations_str)
+
+
+event = {"location": "ashkelon", "information": "cleaning dlila beach", "time": "14:00-16:00", "date": "28/9/2022", "participants": [("noa","0586407344")]}
+event2 = {"location": "ashkelon", "information": "cleaning marina", "time": "17:00-20:00", "date": "25/9/2022", "participants": [("noa","0586407344")]}
+event1 = {"location": "sderot", "information": "plant new trees", "time": "12:00-14:00", "date": "30/9/2022", "participants": [("noa","0586407344")]}
+event4 = {"location": "sderot", "information": "clean parks", "time": "18:00-19:00", "date": "27/9/2022", "participants": [("noa","0586407344")]}
+initialize_file()
+# add_event(event)
+# add_event(event1)
+# add_event(event2)
+# add_event(event4)
+participants = [("Alona", "0527242533"), ("Shoham", "0526225017")]
+add_participants(event, participants)
+#add_participants(event4, participants)
